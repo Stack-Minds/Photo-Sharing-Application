41896258c8875979d9a85e92549d12ec0afc7c98
K.PraharshaSrikar <kadamaripraharshasrikar@gmail.com>
2023-10-10 10:58:11 -0400

    My latest commit

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,50 +1,23 @@
 import React from 'react';
-import {
-  Divider,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-}
-from '@mui/material';
-import './userList.css';
+import { Link } from 'react-router-dom';
 
-/**
- * Define UserList, a React component of project #5
- */
 class UserList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    render() {
+        const users = window.models.userListModel();
 
-  render() {
-    return (
-      <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
-          display your users like so:
-        </Typography>
-        <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
-        </List>
-        <Typography variant="body1">
-          The model comes in from window.models.userListModel()
-        </Typography>
-      </div>
-    );
-  }
+        return (
+            <div className="user-list">
+                <h2>Users</h2>
+                <ul>
+                    {users.map(user => (
+                        <li key={user._id}>
+                            <Link to={`/users/${user._id}`}>{user.first_name} {user.last_name}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default UserList;
