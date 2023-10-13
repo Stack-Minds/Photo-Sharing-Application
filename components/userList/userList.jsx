746cb9c4944b1e4b343bd479da39f95228f591d3
Pallavi Deshmukh <pallavideshmukh32897@gmail.com>
2023-10-13 08:43:09 -0400

    latest commit

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,21 +1,49 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {HashRouter as Router} from 'react-router-dom';
+import fetchModel from '../../lib/fetchModelData.js';
 
 class UserList extends React.Component {
-    render() {
-        const users = window.models.userListModel();
+    constructor(props) {
+        super(props);
+        this.state = { users : [], };
+    }
 
+    addUserName(user) {
+        if (!user) return "";
+        return user.first_name + " " + user.last_name;
+    }
+
+    addList() {
         return (
-            <div className="user-list">
-                <h2>Users</h2>
-                <ul>
-                    {users.map(user => (
-                        <li key={user._id}>
-                            <Link to={`/users/${user._id}`}>{user.first_name} {user.last_name}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+          this.state.users.map((user) =>
+              <ListItem divider={true} key={user._id}>
+                <Link to={"/users/" + user._id} className="user-list-item">
+                  <ListItemText primary={this.addUserName(user)} />
+                </Link>
+              </ListItem>
+          )
+        );
+    }
+
+    componentDidMount() {
+        fetchModel('/user/list')
+          .then((response) => {
+            let users = response['data'];
+            this.setState({ users : users });
+          })
+          .catch((e) => {
+            console.log(e);
+          });
+    }
+
+    render() {
+        return (
+          <Router>
+            <List component="nav">
+              {this.addList()}
+            </List>
+          </Router>
         );
     }
 }
