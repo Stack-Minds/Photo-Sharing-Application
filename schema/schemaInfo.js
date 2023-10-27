28e2505fcc9e30774e2b5a0be6c06e87a90bd02a
Avinash Mallampati <72144928+avinash2906@users.noreply.github.com>
2023-10-27 13:51:19 -0400

    Update schemaInfo.js

--- schema/schemaInfo.js
+++ schema/schemaInfo.js
@@ -8,7 +8,8 @@ const mongoose = require('mongoose');
 const schemaInfo = new mongoose.Schema({
     version: String,
     load_date_time: {type: Date, default: Date.now},
-});
+},
+{ versionKey: false });
 
 /**
  * Create a Mongoose Model.
