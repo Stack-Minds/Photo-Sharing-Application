9ca70963a7fd61e37fa5e4c07de69f8d6846da58
Avinash Mallampati <72144928+avinash2906@users.noreply.github.com>
2023-11-13 16:06:25 -0500

    Update userDetail.jsx

--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,76 +1,67 @@
 import React from 'react';
 import {
-  Typography, Grid, Card, CardContent, Link, TextField, Button
+  Typography, Grid, Card, CardContent, Link, Button
 } from '@mui/material';
 import './userDetail.css';
 import { Link as RouterLink } from 'react-router-dom';
 import axios from 'axios';
 
 class UserDetail extends React.Component {
-  _isMounted = false;
-
   constructor(props) {
     super(props);
-    this.state = { user: {} };
+    this.state = {
+        user: undefined
+    };
   }
-
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
   componentDidMount() {
-    const newId = this.props.match.params.userId;
-    this.handleUserChange(newId);
-}
+      const new_user_id = this.props.match.params.userId;
+      this.handleUserChange(new_user_id);
+  }
 
   componentDidUpdate() {
-    const newId = this.props.match.params.userId;
-    const currentId = this.state.userDetails?._id;
-    if (currentId  !== newId){
-        this.handleUserChange(newId);
-    }
+      const new_user_id = this.props.match.params.userId;
+      const current_user_id = this.state.user?._id;
+      if (current_user_id  !== new_user_id){
+          this.handleUserChange(new_user_id);
+      }
   }
 
-  handleUserChange(userId){
-    axios.get("/user/" + userId)
-        .then((response) =>
-        {
-            const newUser = response.data;
-            this.setState({
-                userDetails: newUser
-            });
-            const main_content = "User Details " + newUser.first_name + " " + newUser.last_name;
-            this.props.changeTopbarContent(main_content);
-        });
+  handleUserChange(user_id){
+      axios.get("/user/" + user_id)
+          .then((response) =>
+          {
+              const new_user = response.data;
+              this.setState({
+                  user: new_user
+              });
+              const main_content = "User Details for " + new_user.first_name + " " + new_user.last_name;
+              this.props.changeMainContent(main_content);
+          });
   }
 
   render() {
-    const { user } = this.state;
-
-    if (!user) {
-      return <Typography variant="h5">User not found</Typography>;
-    }
-
-    return (
+    return this.state.user ? (
       <Card variant="outlined">
         <CardContent>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <Typography variant="h5">{`${user.first_name} ${user.last_name}`}</Typography>
+              <Typography variant="h5">{`${this.state.user.first_name} ${this.state.user.last_name}`}</Typography>
             </Grid>
             <Grid item xs={12}>
-              <Typography variant="body1">Location: {user.location}</Typography>
-              <Typography variant="body1">Description: {user.description}</Typography>
-              <Typography variant="body1">Occupation: {user.occupation}</Typography>
+              <Typography variant="body1">Location: {this.state.user.location}</Typography>
+              <Typography variant="body1">Description: {this.state.user.description}</Typography>
+              <Typography variant="body1">Occupation: {this.state.user.occupation}</Typography>
             </Grid>
             <Grid item xs={12}>
-              <Link component={RouterLink} to={`/photos/${user._id}`}>
+              <Button component={RouterLink} to={`/photos/${this.state.user._id}`}>
                 View Photos
-              </Link>
+              </Button>
             </Grid>
           </Grid>
         </CardContent>
       </Card>
+    ) : (
+        <div/>
     );
   }
 }
