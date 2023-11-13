8ac45f276c129f759a9846160ebc209c17466706
Pallavi Deshmukh <pallavideshmukh32897@gmail.com>
2023-11-13 11:28:59 -0500

    new changes

--- schema/user.js
+++ schema/user.js
@@ -2,9 +2,6 @@
 
 const mongoose = require("mongoose");
 
-/**
- * Define the Mongoose Schema for a Comment.
- */
 const userSchema = new mongoose.Schema({
   first_name: String,
   last_name: String,
@@ -14,12 +11,6 @@ const userSchema = new mongoose.Schema({
 },
 { versionKey: false });
 
-/**
- * Create a Mongoose Model for a User using the userSchema.
- */
 const User = mongoose.model("User", userSchema);
 
-/**
- * Make this available to our application.
- */
 module.exports = User;
